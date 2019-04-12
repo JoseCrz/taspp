234b3127f1e5090e7819e5fb160a49014b722281
José Cuevas <josecueram@gmail.com>
2019-04-12 18:26:22 -0500

    Created Models folder, Created User Model and updated functionality to match the new project structure

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,61 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/taspp-api', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate (value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email format is invalid!')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate (value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password must not include "password" string')
-            }
-        } 
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate (value) {
-            if (value < 0) {
-                throw new Error('Age must be positive!')
-            }
-        }
-    }
-})
-
-const me = new User({
-    name: 'Jotaro',
-    email: 'jotaro@kujo.com',
-    age: 23,
-    password: 'mimibebe'
-})
-
-me.save().then(result => {
-    console.log(result)
-}).catch(error => {
-    console.log(error)
 })
 
 const Task = mongoose.model('Task', {
@@ -70,12 +17,12 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const task = new Task({
-    description: '   defeat DIO    ',
-})
+// const task = new Task({
+//     description: '   defeat DIO    ',
+// })
 
-task.save().then(result => {
-    console.log(result)
-}).catch(error => {
-    console.log(error)
-})
+// task.save().then(result => {
+//     console.log(result)
+// }).catch(error => {
+//     console.log(error)
+// })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
+//NPM Modules
 const express = require('express')
 const chalk = require('chalk')
 
-const PORT = process.env.PORT || 3000
+//Local Modules
+require('./db/mongoose')
+const User = require('./models/userModel')
+
 
 const app = express()
 
+//Server Config
+const PORT = process.env.PORT || 3000
+app.use(express.json())
+
+//Routes
 app.post('/users', (request, response) => {
-    response.send('Testing!')
+    const user = new User(request.body)
+
+    user.save().then(result => {
+        console.log(result)
+        response.send(result)
+    }).catch(error => {
+        response.status(400).send(error)
+    })
 })
 
+//Server Start
 app.listen(PORT, () => {
     console.log(chalk.bgGreen(`Server started on port: ${PORT}`))
 })
