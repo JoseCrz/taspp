3b85fbbaeea4f16b5ec41a1045e5ebacc3e8947d
José Cuevas <josecueram@gmail.com>
2019-04-12 13:57:56 -0500

    Added POST route to create an user

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const chalk = require('chalk')
 
+const PORT = process.env.PORT || 3000
+
 const app = express()
 
-const PORT = process.env.PORT || 3000
+app.post('/users', (request, response) => {
+    response.send('Testing!')
+})
 
 app.listen(PORT, () => {
     console.log(chalk.bgGreen(`Server started on port: ${PORT}`))
